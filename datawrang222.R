4eb11247cc49aa0bf33f48bbd05c3d9c161472cb
Alberto Saucedo <saucedo.publications@gmail.com>
2021-12-20 18:15:34 -0300

    new version 222

---
+++ datawrang222.R
@@ -0,0 +1,9 @@
+#proyecto 1 usando GitHub
+
+#ahora vamos a hacer una pequena alteracion para ver como actualizarlo
+# en github
+# se la, vamos adicionar duas linhas
+# neste archivo
+# y seguimos con la joda
+# y seguimos con mas jodas de los cambios
+ 
